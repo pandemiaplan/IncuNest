acc341bafff8c21a2b4a3f557b50bcab0bba2dbc
pablo18393 <pablosanchez18393@hotmail.com>
2025-10-14 12:01:24 +0200

    Chore: line-endings policy and .gitignore

--- Firmware/motherBoard/include/SPO2.h
+++
@@ -1,6 +0,0 @@
-#include "protocentral_afe44xx.h"
-#include "main.h"
-
-#define SPO2_INIT_TIME 1000
-
-void afe44xx_reset();
--- Firmware/old/motherBoard - Copy/include/ESP32_config.h
+++
@@ -1,8 +0,0 @@
-#include <esp_task_wdt.h>
-
-#include "soc/rtc_cntl_reg.h"
-#include "soc/soc.h"
-
-void watchdogInit(uint32_t wdt_timeout);
-void watchdogReload();
-void brownOutConfig(uint32_t val);
